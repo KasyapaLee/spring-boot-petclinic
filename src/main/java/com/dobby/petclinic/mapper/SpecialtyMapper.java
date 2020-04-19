09826e6504b519c9f5c467a97f53a219189c8faa
liguoqing <lgq0218@gmail.com>
2020-04-19 14:01:15 +0800

    修改vet查询错误

--- src/main/java/com/dobby/petclinic/mapper/SpecialtyMapper.java
+++ src/main/java/com/dobby/petclinic/mapper/SpecialtyMapper.java
@@ -2,7 +2,11 @@ package com.dobby.petclinic.mapper;
 
 import com.dobby.petclinic.common.MyMapper;
 import com.dobby.petclinic.domain.Specialty;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface SpecialtyMapper extends MyMapper<Specialty> {
 
+    List<Specialty> findByVetId(@Param("vetId") int vetId);
 }
